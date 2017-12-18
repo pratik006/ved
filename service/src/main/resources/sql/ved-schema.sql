31ca22b922ea6c9a697531c48cc08114b34db269
Pratik Sengupta <pratik006@gmail.com>
2017-12-18 20:00:52 +0530

    working h2 database with unit testcases

--- service/src/main/resources/sql/ved-schema.sql
+++ service/src/main/resources/sql/ved-schema.sql
@@ -1,5 +1,6 @@
-CREATE SCHEMA  IF NOT EXISTS `ved` /*!40100 DEFAULT CHARACTER SET utf8 */;
-USE `ved`;
+drop schema if exists `ved`;
+create schema `ved`;
+use `ved`;
 -- MySQL dump 10.13  Distrib 5.7.20, for Linux (x86_64)
 --
 -- Host: localhost    Database: ved
@@ -28,12 +29,29 @@ CREATE TABLE `book` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `author_name` varchar(255) DEFAULT NULL,
   `name` varchar(255) DEFAULT NULL,
+  `preview_url` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `UK_wugryet8mf6oi28n00x2eoc4` (`name`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Table structure for table `chapter`
+--
 
+DROP TABLE IF EXISTS `chapter`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `chapter` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `book_id` int(11) DEFAULT NULL,
+  `lang_code` varchar(45) DEFAULT NULL,
+  `headline` varchar(255) DEFAULT NULL,
+  `content` varchar(4000) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `sutra`
@@ -44,22 +62,27 @@ DROP TABLE IF EXISTS `sutra`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `sutra` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `chapter_name` varchar(255) DEFAULT NULL,
-  `chapter_no` int(11) NOT NULL,
+  `chapter_id` int(11) NOT NULL,
   `content` longtext NOT NULL,
   `lang_code` varchar(255) NOT NULL,
   `language` varchar(255) NOT NULL,
-  `verse_no` int(11) NOT NULL,
-  `book_id` bigint(20) DEFAULT NULL,
+  `sutra_no` int(11) NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `FKem4eoslqnos07siqwvjl03qyp` (`book_id`),
-  CONSTRAINT `FKem4eoslqnos07siqwvjl03qyp` FOREIGN KEY (`book_id`) REFERENCES `book` (`id`)
+  KEY `fk_sutra_chapter_idx` (`chapter_id`),
+  CONSTRAINT `fk_sutra_chapter` FOREIGN KEY (`chapter_id`) REFERENCES `chapter` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB AUTO_INCREMENT=7712 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
---
--- Dumping data for table `sutra`
---
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-12-18 19:33:25
 
 --
 -- Table structure for table `commentary`
@@ -80,6 +103,3 @@ CREATE TABLE `commentary` (
 ) ENGINE=InnoDB AUTO_INCREMENT=134010 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `commentary`
---
