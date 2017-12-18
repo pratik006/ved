1188ca61ffcb340dcba63ac182262622b985efee
prasengupta <pratik006@gmail.com>
2017-12-18 16:18:23 +0530

    updated unit testcase

--- service/src/test/java/com/prapps/ved/TestConfigLoader.java
+++ service/src/test/java/com/prapps/ved/TestConfigLoader.java
@@ -8,13 +8,26 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
+
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(classes = {ApplicationStarter.class})
 @ActiveProfiles("local")
 public class TestConfigLoader {
     @Autowired VedService service;
 
-    @Test public void testLoadConfiguration() {
+    @Test public void testLoadConfiguration() { }
 
+    @Test public void testGetBooks() {
+        assertTrue(!service.getBooks().isEmpty());
+    }
+
+    @Test public void testGetBookById() {
+        assertTrue(!service.getBookById(1L).getChapters().isEmpty());
+    }
+
+    @Test public void testGetSutras() {
+        assertEquals(10, service.getSutras(1L, 1, "ro", 0, 10).size());
     }
 }
