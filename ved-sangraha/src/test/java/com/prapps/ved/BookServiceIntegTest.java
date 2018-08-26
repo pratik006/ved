032b29f70f790fc0e087e76da95d3ecca4f05851
Pratik Sengupta <pratik006@gmail.com>
2018-08-26 16:23:33 +0530

    working crud in GAE

---
+++ ved-sangraha/src/test/java/com/prapps/ved/BookServiceIntegTest.java
@@ -0,0 +1,49 @@
+package com.prapps.ved;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+import com.prapps.ved.dto.Book;
+import org.junit.Assert;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.client.RestTemplate;
+
+public class BookServiceIntegTest {
+  private static final String BASE_APP_URI = "https://vedsangraha-187514.appspot.com/";
+  private RestTemplate restTemplate = new RestTemplate();
+
+  @Test
+  public void testCrudOp() throws Exception {
+    Book book = new Book();
+    book.setCode("testing");
+    book.setName("testing name");
+    Book savedBook = restTemplate.postForEntity(BASE_APP_URI+"/books", book, Book.class).getBody();
+    Assert.assertEquals(book.getCode(), savedBook.getCode());
+    Assert.assertEquals(book.getName(), savedBook.getName());
+
+    savedBook = restTemplate.getForEntity(BASE_APP_URI+"/books/"+book.getCode(), Book.class).getBody();
+    Assert.assertEquals(book.getCode(), savedBook.getCode());
+    Assert.assertEquals(book.getName(), savedBook.getName());
+
+    book.setName("new name");
+    restTemplate.put(BASE_APP_URI+"/books/"+book.getCode(), book);
+    savedBook = restTemplate.getForEntity(BASE_APP_URI+"/books/"+book.getCode(), Book.class).getBody();
+    Assert.assertEquals(book.getCode(), savedBook.getCode());
+    Assert.assertEquals(book.getName(), savedBook.getName());
+
+    restTemplate.delete(BASE_APP_URI+"/books/"+book.getCode());
+  }
+}
