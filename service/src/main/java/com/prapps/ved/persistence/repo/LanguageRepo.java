21b515aadf4815ce1a27103b62fca5063f990d94
Pratik Sengupta <pratik006@gmail.com>
2018-08-26 17:43:20 +0530

    removing old service files

--- service/src/main/java/com/prapps/ved/persistence/repo/LanguageRepo.java
+++
@@ -1,9 +0,0 @@
-package com.prapps.ved.persistence.repo;
-
-import com.prapps.ved.persistence.LanguageEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface LanguageRepo extends JpaRepository<LanguageEntity, String> {
-}
